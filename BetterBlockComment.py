f6e87d88df3451122c61f65827efe4a20a01b886
Matthew Morrone <matthewmorrone1@gmail.com>
2015-12-22 20:03:43 -0500

    one of these days I'll have the same settings on all three computers

--- BetterBlockComment.py
+++ BetterBlockComment.py
@@ -1,10 +1,10 @@
 import sublime, sublime_plugin
 
 class BetterBlockCommentCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		view = self.view
-		window = sublime.active_window()
-		window.run_command("expand_selection", {"to" : "line"})
-		window.run_command("toggle_comment", { "block": "true" })
+  def run(self, edit):
+    view = self.view
+    window = sublime.active_window()
+    window.run_command("expand_selection", {"to" : "line"})
+    window.run_command("toggle_comment", { "block": "true" })
 
 
--- ReindentAll.py
+++
@@ -1,8 +0,0 @@
-import sublime, sublime_plugin
-
-class ReindentAllCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		self.view.run_command("select_all")
-		self.view.run_command("reindent")
-		self.view.run_command("invert_selection")
-
